8ad5c9da5f1eff1aaeb16ffa95db5e00c82fe2dd
Ron Tapia <rdt12+git@psu.edu>
2020-02-03 14:22:57 -0500

    Fixed typo

--- scripts/configureSSL.pl
+++ scripts/configureSSL.pl
@@ -39,8 +39,8 @@ my($san) = "san=" . join(",", @sanEntries);
 chdir('/root/shared/tls');
 
 # Generate key.
-echo "SSL KEY NAME: " $name
-echo "SSK KEY SAN:  " $san
+printf("SSL KEY NAME: %s\n", $name);
+printf("SSK KEY SAN:  %s\n" $san);
 system(sprintf("keytool -keystore kafka.server.keystore.jks -alias localhost -validity 365 -genkey -keypass %s -storepass %s -storetype pkcs12 -dname \"cn=%s, ou=scimma-test, o=scimma-test, c=US\" -ext %s >/dev/null 2>/dev/null",
                    $pw, $pw, $name, $san));
 # Create CA
