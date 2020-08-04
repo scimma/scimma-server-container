d050c977af43f87bc3885ff4cfc43980cc84c397
Ron Tapia <rdt12@psu.edu>
2020-08-04 09:39:15 -0400

    added a delay after server start

--- test/test_sec.py
+++ test/test_sec.py
@@ -1,11 +1,13 @@
 import TestUtils as tu
 import re
+import time
 
 def setup_module (module):
     global server
     tu.startNet("scimma-test")
     server = tu.ServerContainer(tu.testTag(), opts=["-v", "shared:/root/shared"])
     server.start()
+    time.sleep(30)
 
 def teardown_module (module):
     server.terminate()
